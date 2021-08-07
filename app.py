f426cf42a175112742f3a88468260d3dac7314e1
unknown <marja.marinic@gmail.com>
2021-08-07 23:31:19 +0100

    Deployment to heroku

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True) # TODO: debug = True should never be used in production. 
+        debug=False) # TODO: debug = True should never be used in production. 
                     # TODO: Change it to False before submitting 
                     # TODO: SECURITY FOULT
     
